e08aef9077030a73bd93c6180913cdf5c46f30ac
lauraberlucchi <laura.berlucchi02@gmail.com>
2023-11-23 07:28:08 +0100

    last prject version

---
+++ SQL queries/course_prices_queries.sql
@@ -0,0 +1,385 @@
+SELECT * FROM sql_project.course_prices;
+
+/* Question: What is average price of course across all schools?*//* Answer: 6353.59)*/
+SELECT 
+    ROUND(AVG(price), 2) AS average_price
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL;
+
+/* Question: What is Average price of each course for all the schools?*//* 
+Answer:# course, school, average_price
+AI Programming with Python, udacity, 1077
+Al Product Manager, udacity, 718
+Android Basics, udacity, 1077
+Android Developer, udacity, 2154
+Artificial Intelligence, udacity, 1077
+Artificial Intelligence Course, brainstation, 3250
+Artificial Intelligence for Trading, udacity, 2154
+Blockchain Developer, udacity, 1436
+Business Analytics, udacity, 1077
+Business Intelligence Analytics, tripleten, 6900
+C++, udacity, 1436
+Cloud Dev Ops Engineer, udacity, 1436
+Cyber Security Bootcamp, ironhack, 7500
+Cyber Security Career Track, springboard, 9900
+Cyber Security Course, syntax-technologies, 0
+Cybersecurity Bootcamp, ccs-learning-academy, 12995
+Cybersecurity Bootcamp, brainstation, 16500
+Cybersecurity Course, brainstation, 3250
+Cybersecurity Part-Time, ironhack, 7500
+Data Analyst Bootcamp (Part-Time), maven-analytics, 7499
+Data Analytics, colaberry, 1999
+Data Analytics, tripleten, 7900
+Data Analytics (Short Course), general-assembly, 4500
+Data Analytics & Business Intelligence Bootcamp, ccs-learning-academy, 7995
+Data Analytics & Business Intelligence Course, syntax-technologies, 0
+Data Analytics Bootcamp, ironhack, 7500
+Data Analytics Career Track, springboard, 8500
+Data Analytics Course, brainstation, 3250
+Data Analytics Flex, thinkful, 8000
+Data Analytics Immersion, thinkful, 12250
+Data Analytics Part-Time, ironhack, 8000
+Data Science, colaberry, 1500
+Data Science, tripleten, 9700
+Data Science & Data Engineering Bootcamp, ccs-learning-academy, 11995
+Data Science Bootcamp, brainstation, 16500
+Data Science Bootcamp (Full-Time), general-assembly, 16450
+Data Science Career Track, springboard, 9900
+Data Science Career Track Prep, springboard, 490
+Data Science Course, brainstation, 3250
+Data Science Flex, thinkful, 9500
+Design Leadership Course, brainstation, 3950
+Digital Marketing (Short Course), general-assembly, 4500
+Digital Marketing Course, brainstation, 3250
+Digital Marketing Flex, thinkful, 4900
+Engineering Flex Program, thinkful, 9500
+Engineering Immersion, thinkful, 16000
+Front-End Web Development, springboard, 0
+Front-End Web Development (Short Course), general-assembly, 4500
+Full-Stack Developer Bootcamp, ccs-learning-academy, 8995
+Introduction to Data Analytics, springboard, 349
+Introduction to Design, springboard, 349
+JavaScript Development (Short Course), general-assembly, 3950
+Marketing Leadership Course, brainstation, 3950
+Product Leadership Course, brainstation, 3950
+Product Management (Short Course), general-assembly, 4500
+Python for Data Analysis, dataquest, 29
+Quality Assurance Engineering, tripleten, 4900
+R for Data Analysis, dataquest, 29
+SDET Automation Testing Course, syntax-technologies, 0
+Software Engineering, tripleten, 9700
+Software Engineering Bootcamp (Full-Time), general-assembly, 16450
+Software Engineering Career Track, springboard, 9900
+Software Engineering Foundations to Core, springboard, 9900
+Tech Sales Career Track, springboard, 5900
+Technical Project Management Flex, thinkful, 6500
+User Experience Design (Short Course), general-assembly, 4500
+User Experience Design Bootcamp (Full-Time), general-assembly, 16450
+UX/UI Design Bootcamp, ironhack, 12000
+UX/UI Design Immersion, thinkful, 12150
+UX/UI Design Part-Time, ironhack, 13000
+UX/UI Flex, thinkful, 9500
+Visual Design (Short Course), general-assembly, 3500
+Web Development Bootcamp, ironhack, 12000
+Web Development Part-Time, ironhack, 13000*/
+
+SELECT 
+    courses, school, ROUND(AVG(price), 2) AS average_price
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL
+GROUP BY school , courses
+ORDER BY course , average_price;
+
+
+/* Question: what is Average price for all courses for school?
+Answer ORDERED BY AVERAGE PRICE (descending), COURSES (ascending)
+Cybersecurity Bootcamp	brainstation	16500
+Data Science Bootcamp	brainstation	16500
+Data Science Bootcamp (Full-Time)	general-assembly	16450
+Software Engineering Bootcamp (Full-Time)	general-assembly	16450
+User Experience Design Bootcamp (Full-Time)	general-assembly	16450
+Engineering Immersion	thinkful	16000
+UX/UI Design Part-Time	ironhack	13000
+Web Development Part-Time	ironhack	13000
+Cybersecurity Bootcamp	ccs-learning-academy	12995
+Data Analytics Immersion	thinkful	12250
+UX/UI Design Immersion	thinkful	12150
+UX/UI Design Bootcamp	ironhack	12000
+Web Development Bootcamp	ironhack	12000
+Data Science & Data Engineering Bootcamp	ccs-learning-academy	11995
+Cyber Security Career Track	springboard	9900
+Data Science Career Track	springboard	9900
+Software Engineering Career Track	springboard	9900
+Software Engineering Foundations to Core	springboard	9900
+Data Science	tripleten	9700
+Software Engineering	tripleten	9700
+Data Science Flex	thinkful	9500
+Engineering Flex Program	thinkful	9500
+UX/UI Flex	thinkful	9500
+Full-Stack Developer Bootcamp	ccs-learning-academy	8995
+Data Analytics Career Track	springboard	8500
+Data Analytics Flex	thinkful	8000
+Data Analytics Part-Time	ironhack	8000
+Data Analytics & Business Intelligence Bootcamp	ccs-learning-academy	7995
+Data Analytics	tripleten	7900
+Cyber Security Bootcamp	ironhack	7500
+Cybersecurity Part-Time	ironhack	7500
+Data Analytics Bootcamp	ironhack	7500
+Data Analyst Bootcamp (Part-Time)	maven-analytics	7499
+Business Intelligence Analytics	tripleten	6900
+Technical Project Management Flex	thinkful	6500
+Tech Sales Career Track	springboard	5900
+Digital Marketing Flex	thinkful	4900
+Quality Assurance Engineering	tripleten	4900
+Data Analytics (Short Course)	general-assembly	4500
+Digital Marketing (Short Course)	general-assembly	4500
+Front-End Web Development (Short Course)	general-assembly	4500
+Product Management (Short Course)	general-assembly	4500
+User Experience Design (Short Course)	general-assembly	4500
+Design Leadership Course	brainstation	3950
+JavaScript Development (Short Course)	general-assembly	3950
+Marketing Leadership Course	brainstation	3950
+Product Leadership Course	brainstation	3950
+Visual Design (Short Course)	general-assembly	3500
+Artificial Intelligence Course	brainstation	3250
+Cybersecurity Course	brainstation	3250
+Data Analytics Course	brainstation	3250
+Data Science Course	brainstation	3250
+Digital Marketing Course	brainstation	3250
+Android Developer	udacity	2154
+Artificial Intelligence for Trading	udacity	2154
+Data Analytics	colaberry	1999
+Blockchain Developer	udacity	1436
+C++	udacity	1436
+Cloud Dev Ops Engineer	udacity	1436
+AI Programming with Python	udacity	1077
+Android Basics	udacity	1077
+Artificial Intelligence	udacity	1077
+Business Analytics	udacity	1077
+Al Product Manager	udacity	718
+Data Science Career Track Prep	springboard	490
+Introduction to Data Analytics	springboard	349
+Introduction to Design	springboard	349
+Python for Data Analysis	dataquest	29
+R for Data Analysis	dataquest	29*/
+
+SELECT 
+    courses, school, ROUND(AVG(price), 2) AS average_price
+FROM
+    course_prices
+GROUP BY school , courses
+ORDER BY average_price DESC , courses ASC;
+
+
+
+/* Question: What are the prices of IronHack courses*//* Answer:# price, course
+7500, Cyber Security Bootcamp
+7500, Cybersecurity Part-Time
+7500, Data Analytics Bootcamp
+8000, Data Analytics Part-Time
+12000, UX/UI Design Bootcamp
+13000, UX/UI Design Part-Time
+12000, Web Development Bootcamp
+13000, Web Development Part-Time
+*/
+
+SELECT 
+    price, courses
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL
+        AND school = 'Ironhack'
+ORDER BY courses , price;
+
+/* Question: What are the prices of springboard courses*//* Answer:# price, course
+9900, Cyber Security Career Track
+8500, Data Analytics Career Track
+9900, Data Science Career Track
+490, Data Science Career Track Prep
+Not found, Front-End Web Development
+349, Introduction to Data Analytics
+349, Introduction to Design
+9900, Software Engineering Career Track
+9900, Software Engineering Foundations to Core
+5900, Tech Sales Career Track
+*/
+SELECT 
+    price, courses
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL
+        AND school = 'springboard'
+ORDER BY courses, price;
+
+/* Question: What are the prices of udacity courses*//* Answer:# price, course
+1077, AI Programming with Python
+718, Al Product Manager
+1077, Android Basics
+2154, Android Developer
+1077, Artificial Intelligence
+2154, Artificial Intelligence for Trading
+1436, Blockchain Developer
+1077, Business Analytics
+1436, C++
+1436, Cloud Dev Ops Engineer
+*/
+
+SELECT 
+    price, courses
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL AND school = 'udacity'
+ORDER BY courses, price;
+
+/* Question: list of courses offered by springboard whose price are less than Ironhack*//* Answer:# # course, school, price
+Data Science Career Track Prep, springboard, 490
+Introduction to Data Analytics, springboard, 349
+Tech Sales Career Track, springboard, 5900
+Introduction to Design, springboard, 349
+*/
+SELECT 
+    courses, school, price
+FROM
+    course_prices
+WHERE
+    school = 'springboard'
+        AND price IS NOT NULL
+        AND price < 7500;
+
+/* Question: list of courses offered by udacity whose price are less than Ironhack*//* Answer:# AI Programming with Python	udacity	1077
+Android Basics	udacity	1077
+Artificial Intelligence	udacity	1077
+Business Analytics	udacity	1077
+Blockchain Developer	udacity	1436
+C++	udacity	1436
+Cloud Dev Ops Engineer	udacity	1436
+Android Developer	udacity	2154
+Artificial Intelligence for Trading	udacity	2154
+Al Product Manager	udacity	718
+*/
+SELECT 
+    courses, school, price
+FROM
+    course_prices
+WHERE
+    school = 'udacity' AND price IS NOT NULL
+        AND price < 7500
+ORDER BY price ASC;
+
+/* question: list of schools providing data analytics courses.*/ /* Answer: # school, course, price
+syntax-technologies, Data Analytics & Business Intelligence Course, Not found
+springboard, Data Analytics Career Track, 8500
+ironhack, Data Analytics Part-Time, 8000
+thinkful, Data Analytics Flex, 8000
+ccs-learning-academy, Data Analytics & Business Intelligence Bootcamp, 7995
+tripleten, Data Analytics, 7900
+ironhack, Data Analytics Bootcamp, 7500
+general-assembly, Data Analytics (Short Course), 4500
+springboard, Introduction to Data Analytics, 349
+brainstation, Data Analytics Course, 3250
+colaberry, Data Analytics, 1999
+thinkful, Data Analytics Immersion, 12250
+*/
+SELECT school,course,price
+FROM course_prices
+WHERE price IS NOT NULL AND course like '%data analytics%'
+order by price desc;
+
+/* Question: list of schools who are providing data analytics courses in less prices than Ironhack.*//* answer: syntax-technologies	Data Analytics & Business Intelligence Course	Not found
+general-assembly	Data Analytics (Short Course)	4500
+springboard	Introduction to Data Analytics	349
+brainstation	Data Analytics Course	3250
+colaberry	Data Analytics	1999*/
+
+SELECT 
+    school, courses, price
+FROM
+    course_prices
+WHERE
+    courses LIKE '%data analytics%'
+        AND price IS NOT NULL
+        AND price < 7500
+ORDER BY price DESC;
+
+/* Question: list of schools who are providing data science courses.*//* Answer: # school, course, price
+springboard, Data Science Career Track, 9900
+tripleten, Data Science, 9700
+thinkful, Data Science Flex, 9500
+springboard, Data Science Career Track Prep, 490
+brainstation, Data Science Course, 3250
+brainstation, Data Science Bootcamp, 16500
+general-assembly, Data Science Bootcamp (Full-Time), 16450
+colaberry, Data Science, 1500   - 4000
+ccs-learning-academy, Data Science & Data Engineering Bootcamp, 11995
+*/
+
+SELECT 
+    school, courses, price
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL
+        AND courses LIKE '%data science%'
+ORDER BY price DESC;
+
+/* Question: list of schools who are providing data science courses in less prices than Ironhack.*//* Answer: # school, course, price
+springboard, Data Science Career Track Prep, 490
+brainstation, Data Science Course, 3250
+colaberry, Data Science, 1500   - 4000
+*/
+SELECT 
+    school, courses, price
+FROM
+    course_prices
+WHERE
+    courses LIKE '%data science%'
+        AND price IS NOT NULL
+        AND price < 7500
+ORDER BY price DESC;
+
+/* Question: list of courses offered by brainstation in less price than IronHack*//* Answer: # course, school, price
+Artificial Intelligence Course, brainstation, 3250
+Cybersecurity Course, brainstation, 3250
+Data Analytics Course, brainstation, 3250
+Data Science Course, brainstation, 3250
+Design Leadership Course, brainstation, 3950
+Digital Marketing Course, brainstation, 3250
+Marketing Leadership Course, brainstation, 3950
+Product Leadership Course, brainstation, 3950
+*/
+SELECT 
+    courses, school, price
+FROM
+    course_prices
+WHERE
+    school = 'brainstation'
+        AND price IS NOT NULL
+        AND price < 7500;
+
+/* Question: list of courses offered by brainstation.*//* Answer:# course, price
+Artificial Intelligence Course, 3250
+Cybersecurity Bootcamp, 16500
+Cybersecurity Course, 3250
+Data Analytics Course, 3250
+Data Science Bootcamp, 16500
+Data Science Course, 3250
+Design Leadership Course, 3950
+Digital Marketing Course, 3250
+Marketing Leadership Course, 3950
+Product Leadership Course, 3950
+ */
+SELECT 
+    courses, price
+FROM
+    course_prices
+WHERE
+    price IS NOT NULL
+        AND school = 'brainstation'
+ORDER BY courses , price;
---
+++ SQL queries/locations_table_queries.sql
@@ -0,0 +1,238 @@
+SELECT 
+    *
+FROM
+    sql_project.locations;
+    
+
+/* question: In which country schools are providing online courses?*//* Answer: no data is available*/
+SELECT 
+    *
+FROM
+    locations
+WHERE
+    description_loc LIKE '%online%';
+
+SELECT DISTINCT school
+FROM locations
+WHERE description_loc LIKE '%online%';
+
+/* question: which schools are providing maximum online courses?*//* answer: # every school provides online couses.*/
+SELECT 
+    school, COUNT(*) AS online_course_count
+FROM
+    locations
+WHERE
+    description_loc LIKE 'Online'
+GROUP BY school
+ORDER BY online_course_count DESC;
+
+/* question: which cities are the most popular across all schools.*//* Answer: Online-12, London-2, Miami-2,NYC-2, Toronto-2*/
+SELECT 
+    city_name, COUNT(*) AS popularity
+FROM
+    locations
+GROUP BY city_name
+ORDER BY popularity DESC, city_name ASC
+LIMIT 5;
+
+/* question: which country are the most popular across all schools.*//* Answer: Online-12, United States-6,Canada-3,Spain-2,United Kingdom-2,Spain-2*/
+SELECT 
+    country_name, COUNT(*) AS popularity
+FROM
+    locations
+WHERE
+    country_name IS NOT NULL
+GROUP BY country_name
+ORDER BY popularity DESC;
+
+/* QUESTION: Which are the top 5 courses and in which country are they most popular*//* Answer:# course_name, country_name, popularity
+Web Development Bootcamp, United States, 5
+Design Leadership Course, United States, 4
+Data Science Course, United States, 4
+Artificial Intelligence Course, United States, 4
+Data Analytics Course, United States, 4
+*/
+SELECT 
+    *
+FROM
+    courses_table;
+SELECT 
+    *
+FROM
+    locations;
+SELECT 
+    c.courses AS course_name,
+    l.country_name,
+    COUNT(*) AS popularity
+FROM
+    courses c
+        JOIN
+    locations l ON c.school_id = l.school_id
+GROUP BY course_name , country_name
+ORDER BY popularity DESC
+LIMIT 5;
+
+/* Question: Which are the top 10 cities in which IronHack offers all the courses*//* Answer:Lisbon-8,Barcelona-8,Madrid-8,
+Miami-8,Paris-8, Sao Paulo-8, Amsterdam-8, Mexico City-8,Berlin-8*/
+
+SELECT 
+    l.city_name, COUNT(c.courses) AS course_count
+FROM
+    locations l
+        JOIN
+    courses c ON l.school_id = c.school_id
+WHERE
+    l.city_name IS NOT NULL
+        AND c.school = 'Ironhack'
+GROUP BY city_name
+LIMIT 10;
+
+/* Question: Are these cities the a subset of all cities with ironhack couses?*//* no, these are no more cities where ironhack is present at the moment.*/
+SELECT 
+    l.city_name, COUNT(c.courses) AS course_count
+FROM
+    locations l
+        JOIN
+    courses c ON l.school_id = c.school_id
+WHERE
+    l.city_name IS NOT NULL
+        AND c.school = 'Ironhack'
+GROUP BY city_name;
+
+/* Question: Which are the countries in which IronHack offers less courses*//* Answer: # country_name, course_count
+Germany, 8
+Mexico, 8
+Netherlands, 8
+Brazil, 8
+France, 8
+United States, 8
+Portugal, 8
+Spain, 16
+*/
+SELECT 
+    l.country_name,
+    COUNT(c.courses) AS course_count
+FROM
+    locations l
+        JOIN
+    courses c ON l.school_id = c.school_id
+WHERE
+    l.country_name IS NOT NULL
+        AND c.school = 'Ironhack'
+GROUP BY country_name
+ORDER BY course_count ASC;
+
+/* Qusetion: list of countries where IronHack is not providing any course.*/ 
+/* Answer: 
+Online	Online
+United States	New York
+Canada	Ontario
+Canada	British Columbia
+United States	Massachusetts
+United States	Illinois
+United Kingdom	United Kingdom
+United States	Florida*/
+
+
+SELECT DISTINCT
+    l.country_name, l.state_name
+FROM
+    locations l
+        JOIN
+    courses c ON l.school_id = c.school_id
+WHERE
+    c.school != 'Ironhack';
+
+/* Question: list of countries where Ironhack offers data analytics courses.*/ /* Answer: # course_name, country_name
+Data Analytics Bootcamp, France
+Data Analytics Bootcamp, Brazil
+Data Analytics Bootcamp, Netherlands
+Data Analytics Bootcamp, Mexico
+Data Analytics Bootcamp, Germany
+Data Analytics Part-Time, Portugal
+Data Analytics Part-Time, Spain
+Data Analytics Part-Time, United States
+Data Analytics Part-Time, France
+Data Analytics Part-Time, Brazil
+Data Analytics Part-Time, Netherlands
+Data Analytics Part-Time, Mexico
+Data Analytics Part-Time, Germany
+*/
+SELECT 
+    courses.courses AS course_name,
+    locations.country_name
+FROM
+    courses
+        JOIN
+    locations ON courses.school_id = locations.school_id
+WHERE
+    courses.courses LIKE '%data analytics%'
+        AND locations.country_name IS NOT NULL
+        AND locations.school = 'Ironhack'
+GROUP BY course_name, country_name;
+
+/* Question: list of other countries that offers data analytics courses.*/ /* answer: # school_name, course_name, country_name
+brainstation, Data Analytics Course, United States
+brainstation, Data Analytics Course, Canada
+brainstation, Data Analytics Course, United Kingdom
+general-assembly, Data Analytics Bootcamp (Full-Time), United States
+general-assembly, Data Analytics (Short Course), United States
+general-assembly, Data Analytics Bootcamp (Full-Time), United Kingdom
+general-assembly, Data Analytics (Short Course), United Kingdom
+general-assembly, Data Analytics Bootcamp (Full-Time), Canada
+general-assembly, Data Analytics (Short Course), Canada
+*/
+SELECT 
+    courses.school AS school_name,
+    courses.courses AS course_name,
+    locations.country_name
+FROM
+    courses
+        JOIN
+    locations ON courses.school_id = locations.school_id
+WHERE
+    courses.courses LIKE '%data analytics%'
+        AND locations.country_name IS NOT NULL
+        AND locations.school != 'Ironhack'
+GROUP BY school_name , course_name , country_name;
+
+/* Question: list of countries where other schools offers data science courses.*//* Answer: brainstation	Data Science Course	United States
+brainstation	Data Science Bootcamp	United States
+brainstation	Data Science Course	Canada
+brainstation	Data Science Bootcamp	Canada
+brainstation	Data Science Course	United Kingdom
+brainstation	Data Science Bootcamp	United Kingdom
+general-assembly	Data Science Bootcamp (Full-Time)	United States
+general-assembly	Data Science (Short Course)	United States
+general-assembly	Data Science Bootcamp (Full-Time)	United Kingdom
+general-assembly	Data Science (Short Course)	United Kingdom
+general-assembly	Data Science Bootcamp (Full-Time)	Canada
+general-assembly	Data Science (Short Course)	Canada*/
+SELECT 
+    courses.school AS school_name,
+    courses.courses AS course_name,
+    locations.country_name
+FROM
+    courses
+        JOIN
+    locations ON courses.school_id = locations.school_id
+WHERE
+    courses.courses LIKE '%data science%'
+        AND locations.country_name IS NOT NULL
+        AND locations.school != 'Ironhack'
+GROUP BY school_name , course_name , country_name;
+
+/* Question: list of countries where Ironhack offers data science courses.*//* Answer: No record is available*/
+SELECT 
+    courses.school AS school_name,
+    courses.courses AS course_name,
+    locations.country_name
+FROM
+    courses
+        JOIN
+    locations ON courses.school_id = locations.school_id
+WHERE
+    courses.courses LIKE '%data science%'
+        AND locations.country_name IS NOT NULL
+        AND locations.school = 'Ironhack'
+GROUP BY school_name , course_name , country_name;
